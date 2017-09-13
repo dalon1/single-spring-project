45a09ba71c6d8bad4027097788beb11c09c7e49e
Dannel Alon <dannelvision@gmail.com>
2017-09-13 19:17:48 -0400

    My First success in spring framework

---
+++ first-service/src/main/java/com/firstservice/app/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.firstservice.app.controller;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstservice.app.model.Greeting;
+
+@RestController // @Controller + @ResponseBody
+public class GreetingController {
+
+	public static final String template = "Hello, %s!";
+	
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
+	public Greeting getGreeting(@RequestParam(
+			value = "name", defaultValue = "World") String name) {
+		Greeting greeting = new Greeting();
+		greeting.setId(new AtomicInteger().incrementAndGet());
+		greeting.setContent(String.format(template, name));
+		return greeting;
+	}
+}
